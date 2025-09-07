ed3a3c869ba600d7263b53ba60218b51d8c45104
iriss <iriss@GALAXYBOOK2-4>
2025-09-07 12:05:57 -0300

    Agenda3

--- MauiAppMinhasComprass/App.xaml.cs
+++ MauiAppMinhasComprass/App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MauiAppMinhasComprass
+﻿using MauiAppMinhasComprass.Helpers;
+
+namespace MauiAppMinhasComprass
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string dbPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3"
+                    );
+                    _db = new SQLiteDatabaseHelper(dbPath);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
